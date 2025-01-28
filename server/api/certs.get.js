1d1b94e22e5eae497adf7c5fb54fe522b93d8ae0
Samith Seu <59063392+samithseu@users.noreply.github.com>
2025-01-28 11:08:36 +0700

    Fetching data without `notion` package

--- server/api/certs.get.js
+++ server/api/certs.get.js
@@ -1,46 +1,50 @@
-import { Client } from "@notionhq/client";
-
 export default defineCachedEventHandler(
   async (event) => {
     const config = useRuntimeConfig(event);
-    const notion = new Client({ auth: config.notionApiKey });
-    let allCertificates = [];
     try {
-      const response = await notion.databases.query({
-        database_id: config.notionDbId,
-        filter: {
-          and: [
-            {
-              property: "Org",
-              select: {
-                equals: "BTI",
-              },
+      const response = await fetch(
+        `https://api.notion.com/v1/databases/${config.notionDbId}/query`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${config.notionApiKey}`,
+            "Notion-Version": "2022-06-28",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            filter: {
+              and: [
+                {
+                  property: "Org",
+                  select: {
+                    equals: "BTI",
+                  },
+                },
+                {
+                  property: "Given",
+                  checkbox: {
+                    equals: true,
+                  },
+                },
+              ],
             },
-            {
-              property: "Given",
-              checkbox: {
-                equals: true,
-              },
-            },
-          ],
-        },
-      });
-
-      allCertificates = response.results.map((res) => {
+          }),
+        }
+      );
+      const data = await response.json();
+      return data.results.map((res) => {
         let id = res.properties.ID.rich_text[0].plain_text;
         let subject = res.properties.Subject.title[0].plain_text;
         let photoURL = res.properties.URL.url;
         return { id, subject, photoURL };
       });
-
-      return allCertificates;
     } catch (error) {
-      console.log(error);
+      console.error(error);
       return [];
     }
   },
   {
-    maxAge: 60 * 5, //5 minutes,
+    maxAge: 60 * 2, //2 minutes,
     staleMaxAge: 30,
     swr: true,
   }
